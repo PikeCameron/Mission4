f89a721b20c24891a80f5437ad60e2df84202679
CameronPike <cp475@byu.edu>
2023-02-06 16:07:13 -0700

    Mission5

---
+++ Mission4/Models/CalculatorModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission4.Models
+{
+    public class CalculatorModel
+    {
+        // get and set form variables, must be between 0 and 100
+        [Range(0,100, ErrorMessage = "Must be between 0 and 100")]
+        public int assignments { get; set; }
+        [Range(0, 100, ErrorMessage = "Must be between 0 and 100")]
+        public int group_projects { get; set; }
+        [Range(0, 100, ErrorMessage = "Must be between 0 and 100")]
+        public int quizzes { get; set; }
+        [Range(0, 100, ErrorMessage = "Must be between 0 and 100")]
+        public int midterm_exam { get; set; }
+        [Range(0, 100, ErrorMessage = "Must be between 0 and 100")]
+        public int final_exam { get; set; }
+        [Range(0, 100, ErrorMessage = "Must be between 0 and 100")]
+        public int intex { get; set; }
+
+    }
+}
